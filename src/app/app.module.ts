7378f47d3ba9a6a0539a864994156a6247ec3a1c
Lyulko Alexander <lyulko.alexander@gmail.com>
2018-09-29 14:36:44 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { CabinetModule } from './cabinet/cabinet.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {CabinetModule} from './cabinet/cabinet.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+//hhh
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    CabinetModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CabinetModule,
+        MatButtonModule,
+        MatCheckboxModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/cabinet/cabinet.module.ts
+++ src/app/cabinet/cabinet.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { TaskDashboardComponent } from './containers/task-dashboard/task-dashboard.component';
+import {NgModule} from '@angular/core';
+import {TaskDashboardComponent} from './containers/task-dashboard/task-dashboard.component';
+import {MenubarComponent} from './containers/menubar/menubar.component';
 import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
-  declarations:[
-    TaskDashboardComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  exports: [
-    TaskDashboardComponent
-  ]
+    declarations: [
+        TaskDashboardComponent,
+        MenubarComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    exports: [
+        TaskDashboardComponent,
+        MenubarComponent
+    ]
 })
 export class CabinetModule {
 
