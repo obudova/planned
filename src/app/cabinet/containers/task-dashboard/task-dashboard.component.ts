7ccb557d676f3032bd85b96a9c2ac6c3271bb6bc
Lyulko Alexander <lyulko.alexander@gmail.com>
2018-09-30 13:42:46 +0300

    Merge from diff

--- src/app/cabinet/containers/task-dashboard/task-dashboard.component.ts
+++ src/app/cabinet/containers/task-dashboard/task-dashboard.component.ts
@@ -39,13 +39,32 @@ export class TaskDashboardComponent implements OnInit, OnChanges {
 
     openCreateTaskModal() {
         const dialogRef = this.dialog.open(TaskFormComponent, {
-            width: '250px'
+            width: '250px',
+            data: {
+                projects: this.projects.map(project => {
+                    return {
+                        value: project.id,
+                        viewValue: project.title
+                    };
+                })
+            }
         });
 
         dialogRef.afterClosed().subscribe(result => {
-            console.log('The dialog was closed');
-            // this.animal = result;
+            if (result && result.project) {
+                this.addTaskToProject(result);
+            }
         });
+    }
+
+    addTaskToProject(task) {
+        this.taskService.createTask({
+            ...task,
+            due_date: moment(task.due_date).valueOf()
+        }, this.tasks.length + 1)
+            .subscribe((response) => {
+                this.loadAppropriateTasks(this.filterForm.get('filter').value);
+            });
     }
 
     listAll() {
@@ -55,6 +74,7 @@ export class TaskDashboardComponent implements OnInit, OnChanges {
                 this.processTasks();
             });
     }
+
     findByString(str: String) {
         this.taskService.findTasksByString(this.searchQuery)
             .subscribe((res) => {
@@ -68,6 +88,7 @@ export class TaskDashboardComponent implements OnInit, OnChanges {
                 this.processTasks();
             });
     }
+
     listPersonal() {
         this.taskService.listPersonalTasks()
             .subscribe((res) => {
@@ -91,24 +112,35 @@ export class TaskDashboardComponent implements OnInit, OnChanges {
 
         this.filterForm.get('filter').valueChanges
             .subscribe((value) => {
-                switch (value) {
-                    case 'personal': {
-                        this.listPersonal();
-                        break;
-                    }
-                    case 'all': {
-                        this.listAll();
-                        break;
-                    }
-                    case 'project': {
-                        this.taskService.listProjectTasks()
-                            .subscribe((res) => {
-                                this.tasks = res;
-                                this.processTasks();
-                            });
-                    }
-                }
+                this.loadAppropriateTasks(value);
             });
+    }
+
+    loadAppropriateTasks(value) {
+        switch (value) {
+            case 'personal': {
+                this.listPersonal();
+                break;
+            }
+            case 'all': {
+                this.listAll();
+                break;
+            }
+            case 'project': {
+                this.taskService.listProjectTasks()
+                    .subscribe((res) => {
+                        this.tasks = res;
+                        this.processTasks();
+                    });
+            }
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes && changes.searchQuery) {
+            console.log('chaned query', this.searchQuery);
+            this.findByString(this.searchQuery);
+        }
     }
 
     processTasks() {
@@ -116,11 +148,5 @@ export class TaskDashboardComponent implements OnInit, OnChanges {
         const tomorrow = this.today.clone().add(1, 'day');
         this.tasksForTommorrow = this.tasks.filter(task => moment(task.due_date).dayOfYear() === tomorrow.clone().dayOfYear());
         this.leftTasks = this.tasks.filter(task => moment(task.due_date).valueOf() > tomorrow.clone().endOf('day').valueOf());
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes && changes.searchQuery) {
-            this.findByString(this.searchQuery);
-        }
     }
 }
